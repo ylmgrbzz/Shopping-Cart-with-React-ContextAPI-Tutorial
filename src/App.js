a80e3ec1a0cb3aa7bea84cd2a8989705125519cf
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-29 10:44:48 +0300

    ContextAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from "react";
+import { createContext } from "react";
 import "./style.css";
 import { Route } from "react-router-dom";
-
+import { useState } from "react";
 import Products from "./components/Products";
 import Cardd from "./components/Cardd";
 
-// import { data } from "./data";
+import { data } from "./data";
+
+export const BooksContext = createContext();
 
 export default function App() {
+  const [state, setState] = useState({
+    bookList: data,
+    cart: [],
+  });
   return (
-    <div className="App">
-      <h1>
-        Alışveriş Sepeti Yapımı
-        <img
-          src="https://avatars3.githubusercontent.com/u/60869810?v=4"
-          alt="React Dersleri"
-        />{" "}
-        React Dersleri
-      </h1>
-      <Route exact path="/" component={Products} />
-      <Route path="/cart" component={Cardd} />
-    </div>
+    <BooksContext.Provider value={state}>
+      <div className="App">
+        <h1>
+          Alışveriş Sepeti Yapımı
+          <img
+            src="https://avatars3.githubusercontent.com/u/60869810?v=4"
+            alt="React Dersleri"
+          />{" "}
+          React Dersleri
+        </h1>
+        <Route exact path="/" component={Products} />
+        <Route path="/cart" component={Cardd} />
+      </div>
+    </BooksContext.Provider>
+
   );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,29 @@
+import { useContext } from "react";
 import React from "react";
 import { Link } from "react-router-dom";
+import { BooksContext } from "../App";
+
 
 const Products = props => {
+    const context = useContext(BooksContext)
+    console.log(context)
     return (
         <div>
             <h2>
                 <span>Kitap Listesi</span>
                 <Link to="/cart">Sepetim</Link>
             </h2>
-            <div className="book">
-                <img
-                    src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
-                    alt="Simyaci"
-                />
-                <div>
-                    <h4>Simyaci</h4>
-                    <p>Yazar: Paulo Coelho</p>
-                    <p>Fiyat: &#8378; 19.99</p>
-                    <button>Sepete Ekle</button>
+            {context.bookList.map(book => (
+                <div className="book" key={book.id}>
+                    <img src={book.image} alt={book.name} />
+                    <div>
+                        <h4>{book.name}</h4>
+                        <p>Yazar: {book.author}</p>
+                        <p>Fiyat: &#8378;{book.price}</p>
+                        <button>Sepete Ekle</button>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 };
